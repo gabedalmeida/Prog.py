41a968ec1f2f5cebfe3e6227bb3070306e31b8d4
Gabriel Almeida <gabrelalmeida17@icloud.com>
2023-09-08 16:28:28 -0400

    Colocando as primeiras escolhas

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Sistema de conversao de medidas')
+escolha = int(input('Qual opcao voce deseja escolher? [1]Temperatura [2]Distancia [3]Peso [4]Moeda'))
+if escolha == 1:
+    print('Voce deseja trocar a medida da Temperatura.')
+elif escolha == 2:
+    print('Voce deseja trocar a medida da Distancia.')
+elif escolha == 3:
+    print('Voce deseja trocar a medida da Peso.')
+elif escolha == 4:
+    print('Voce deseja trocar a medida da Moeda.')
+else:
+    print('Escolha errada, nao há essa opcao')
+     
